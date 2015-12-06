5fcf4896ac5b10c1977663d2eb98dea48df98c7c
TastyBytes <mechanicalmen.tastybytes@gmail.com>
2015-12-06 12:04:32 +0100

    FileLineInfo added.

--- source/Mechanical3.Portable/GlobalSuppressions.cs
+++ source/Mechanical3.Portable/GlobalSuppressions.cs
@@ -3,11 +3,13 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1000:Keywords must be spaced correctly", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1008:Opening parenthesis must be spaced correctly", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1009:Closing parenthesis must be spaced correctly", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements must appear in the correct order", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements must appear in the correct order", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:Braces must not be omitted", Justification = "<Pending>")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1000:Keywords must be spaced correctly")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1008:Opening parenthesis must be spaced correctly")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1009:Closing parenthesis must be spaced correctly")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements must appear in the correct order")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:Elements must be ordered by access")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression must have justification")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:Braces must not be omitted")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1003:Symbols must be spaced correctly")]
---
+++ source/Mechanical3.Tests/Misc/FileLineInfoTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Mechanical3.Misc;
+using NUnit.Framework;
+
+namespace Mechanical3.Tests.Misc
+{
+    public static class FileLineInfoTests
+    {
+        [Test]
+        public static void DefaultParameterTest()
+        {
+            var info = FileLineInfo.Create();
+            Assert.True(string.Equals("FileLineInfoTests.cs", info.File, StringComparison.Ordinal));
+            Assert.True(string.Equals("DefaultParameterTest", info.Member, StringComparison.Ordinal));
+            Assert.AreEqual(12, info.Line);
+            Assert.True(string.Equals("   at DefaultParameterTest in FileLineInfoTests.cs:line 12", info.ToString(), StringComparison.Ordinal));
+        }
+
+        [Test]
+        public static void BadParameterTest()
+        {
+            Assert.Throws<ArgumentException>(() => new FileLineInfo(null, "a", 1));
+            Assert.Throws<ArgumentException>(() => new FileLineInfo("a", null, 1));
+            Assert.Throws<ArgumentException>(() => new FileLineInfo("a", "b", -1));
+            var info = new FileLineInfo(string.Empty, string.Empty, 0); // works
+
+            Assert.True(string.Equals("   at ? in ?:line 0", info.ToString(), StringComparison.Ordinal));
+        }
+    }
+}
